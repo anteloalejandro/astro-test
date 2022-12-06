6b0fc411c0c706aa7c1db9156f30302c73bff1ce
Alejandro Antelo <alfasant@protonmail.com>
2022-12-06 16:33:43 +0100

    Added RSS functionality

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,9 @@
+import rss from '@astrojs/rss'
+
+export const get = () => rss({
+  title: 'Astro test page',
+  description: 'My journey testing Astro',
+  site: 'https://antelo-astro.netify.com',
+  items: import.meta.glob('./posts/**/*.md'),
+  customData: '<language>es</language>'
+})
