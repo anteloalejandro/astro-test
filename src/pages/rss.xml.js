95d42a1a7395461a761d8fc8dd372fa819243a45
Alejandro Antelo <alfasant@protonmail.com>
2022-12-06 18:32:21 +0100

    fixed typo

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss'
 export const get = () => rss({
   title: 'Astro test page',
   description: 'My journey testing Astro',
-  site: 'https://antelo-astro.netify.app',
+  site: 'https://antelo-astro.netlify.app',
   items: import.meta.glob('./posts/**/*.md'),
   customData: '<language>es</language>'
 })
