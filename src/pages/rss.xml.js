dde43cece4ca91a3618e9a1df826ca118ffdf35c
Alejandro Antelo <alfasant@protonmail.com>
2022-12-06 18:30:28 +0100

    fixed website url

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,7 +3,7 @@ import rss from '@astrojs/rss'
 export const get = () => rss({
   title: 'Astro test page',
   description: 'My journey testing Astro',
-  site: 'https://antelo-astro.netify.com',
+  site: 'https://antelo-astro.netify.app',
   items: import.meta.glob('./posts/**/*.md'),
   customData: '<language>es</language>'
 })
